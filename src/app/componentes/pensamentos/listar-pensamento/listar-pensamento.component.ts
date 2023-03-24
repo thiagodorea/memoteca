930dfbe2b8e561e2fc4aaa861f7fa9bedafcfaf5
thiagodorea <thiago.dorea@hotmail.com>
2023-03-23 23:34:03 -0300

    Cria o carregemento por demanda

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -10,13 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
+  paginaAtual: number = 1;
+  haMaisPensamentos:boolean = true;
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar().subscribe((pensamentos) => {
+    this.service.listar(this.paginaAtual).subscribe((pensamentos) => {
       this.listaPensamentos = pensamentos;
     });
+  }
+
+  carregarMaisPensamentos(){
+    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if(!listaPensamentos.length){
+        this.haMaisPensamentos = false;
+      }
+    })
   }
 
 }
